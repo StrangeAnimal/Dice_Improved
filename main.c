808635153a74a30f3c42289a131b5fcba9784190
StrangeAnimal <33088899+StrangeAnimal@users.noreply.github.com>
2017-11-21 10:29:44 +0000

    added comments

--- main.c
+++ main.c
@@ -5,16 +5,16 @@
 
 
 char top [2];
-char mid [3];
+char mid [3]; //arrays to stor the variable charachters for the top middle and bottom line
 char bottom [2];
 
 
 
 
 void dice(char top[],char mid[],char bottom[],int numdice,int spotnum[]){
-for(int x=0;x< numdice;x++)    
+for(int x=0;x< numdice;x++)    //prints the line as many times as needed
 printf("   +---------+       "); 
-printf("\n");
+printf("\n"); //newline must be here to allow prior line to be printed n times on the same line
 for(int x=0;x< numdice;x++)
 printf("  /         /|       ");
 printf("\n");
@@ -25,8 +25,8 @@ for(int x=0;x< numdice;x++)
 printf("+---------+  |       ");
 printf("\n");
 for(int x=0;x< numdice;x++){
- setdice(spotnum[x]);   
-printf("|%c       %c|  |       ",top[0],top[1]);
+ setdice(spotnum[x]); //set the spot charchters according to the number there should be
+printf("|%c       %c|  |       ",top[0],top[1]); //print the line
 }
 
 printf("\n");
@@ -54,13 +54,13 @@ printf("\n");
 
 
 int random(){
- int r = rand() % ((5 +1 - 0) + 0);
+ int r = rand() % ((5 +1 - 0) + 0); //generate a random number between 0 and 5
     
     return r;
     }
 
 void setdice(int x){
-    switch (x){
+    switch (x){ //set the number of spots in the desired pattern
         
       case 0 :
       top[0] = ' ';
@@ -146,7 +146,13 @@ void setdice(int x){
 int main(int argc, char **argv)
 {
     
-    int numdice = 5; //declare number of dice to print
+    printf("enter the number of dice per row\n");
+    
+    
+    
+    int numdice;//declare number of dice to print
+    scanf("%d",& numdice); //take in the number and assign it to spotnum
+    system("cls"); //clear the screen
     
    int spotnum [numdice]; //array for random numbers of spots for dice
     
@@ -157,7 +163,7 @@ int main(int argc, char **argv)
     srand(time(NULL));
     while(1){
     for(int x=0; x<numdice;x++){  //generates number of spots for each dice
-        spotnum[x] = random();
+        spotnum[x] = random(); //assign random number
         }
     
     
@@ -165,12 +171,12 @@ int main(int argc, char **argv)
     
     
     
-     dice(top,mid,bottom,numdice,spotnum);
+     dice(top,mid,bottom,numdice,spotnum); //print the dice
         
 
    
     
-    getch();
+    getch(); //loop on a key press (waits for a key press before looping)
     
     }
 
